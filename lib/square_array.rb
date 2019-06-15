f4dbd1d9e7def835b3c8eb1a1a72044dc5136450
Audrea Cook <audreacwork@gmail.com>
2019-06-15 21:28:00 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-  new_array = []
-  counter = 0
-  while counter < array.size do
-    new_array << array[counter] ** 2
-    counter += 1
+  new_array = [] # create empty array
+  i = 0
+  while array[i] do
+    new_array << array[i] ** 2 # shovel loop results into empty array
+    i += 1
   end
   new_array
 end
