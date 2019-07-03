a9581ea207e494c3642128a50cc209e959a553c1
Audrea Cook <audreacwork@gmail.com>
2019-07-03 19:47:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,15 @@
 def square_array(array)
+<<<<<<< HEAD
   new_array = [] # create empty array
   i = 0
   while array[i] do
     new_array << array[i] ** 2 # shovel loop results into empty array
+=======
+  new_array = square_array(array)
+  i = 0
+  while array[i] do
+    array[i] ** 2
+>>>>>>> 3f8460cd409d30956164e57056d4f972e2517559
     i += 1
   end
   new_array
