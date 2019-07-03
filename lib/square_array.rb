8ce3e7ce4f5ad907db8a1a7cc3fa7f82226f64fd
Audrea Cook <audreacwork@gmail.com>
2019-07-03 19:55:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,6 +3,8 @@ def square_array(array)
   squared = []
   while array[count] do
     squared << (array[count] * array[count])
+    # or
+    # squared << (array[count] ** 2)
     count += 1
   end
   squared
