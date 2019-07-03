6df84d4776b3442f74cdaf84d4dcc85d38ec9381
Audrea Cook <audreacwork@gmail.com>
2019-07-03 19:55:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 0
   squared = []
   while array[count] do
-    new_array << (array[count] * array[count])
+    squared << (array[count] * array[count])
     i += 1
   end
   squared
